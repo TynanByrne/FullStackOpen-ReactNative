7495e5fe4b3448e91e0422442b74f1483c8fc7ce
Tynan Byrne <tsbyrne6@gmail.com>
2020-11-24 22:24:22 +0000

    Added test for form

---
+++ rate-repository-app/src/components/SignIn/SignInContainer.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import SignInContainer from './SignInContainer';
+// ...
+
+describe('SignIn', () => {
+  describe('SignInContainer', () => {
+    it('calls onSubmit function with correct arguments when a valid form is submitted', async () => {
+
+      const styles = {
+        form: { color: 'red' },
+      }
+      onSubmit = jest.fn();
+      const { getByPlaceholderText, getByText } = render(
+        <SignInContainer onSubmit={onSubmit} styles={styles} />
+      );
+
+      const username = getByPlaceholderText('Username');
+      const password = getByPlaceholderText('Password');
+      fireEvent.changeText(username, 'kalle');
+      fireEvent.changeText(password, 'password');
+      fireEvent.press(getByText('Sign in'));
+
+      await waitFor(() => {
+        // expect the onSubmit function to have been called once and with a correct first argument
+        expect(onSubmit).toHaveBeenCalledTimes(1);
+        expect(onSubmit.mock.calls[0][0]).toEqual({
+          username: 'kalle',
+          password: 'password',
+        });
+      });
+    });
+  });
+});
